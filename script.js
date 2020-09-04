53d34fbaca4a4ebf0c4ee8bbd6ae70434af9da05
Natalie Roy <nataliedanielleroy@gmail.com>
2020-09-03 23:22:22 -0700

    testing tab pagination

---
+++ script.js
@@ -0,0 +1,22 @@
+// a function to create tabs on my portfolio site!
+// first time doing something like this, heavily copied from w3schools
+function openTab(evt, tabName) {
+  // variables
+  var i, tabcontent, tablinks;
+
+  // hide tabs
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // collect elements and remove active class
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tabcontent.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace("active", "");
+  }
+
+  // show sleected tab and declare it active
+  document.getElementById(tabName).style.display = "block";
+  evt.currentTarget.className += "active";
+}
